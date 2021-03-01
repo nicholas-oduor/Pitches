0a421c9a8e365cadd8ba762f156262bcbd242d77
nicholas-oduor <oduor5742@gmail.com>
2021-03-01 15:52:51 +0300

    added class Pitchform

---
+++ app/main/forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SubmitField,SelectField
+from wtforms.validators import Required
+
+
+class PitchForm(FlaskForm):
+    title = StringField('Pitch Title')
+    category = SelectField(u'Pitch Categories', choices=[('Product', 'Product'), ('Adverts', 'Adverts'),('Interview', 'Interview')])
+    pitch = TextAreaField('Pitch')
+    submit = SubmitField('Submit')
