80b27a1fcde0920afc4490007e8f6e02916b775b
nicholas-oduor <oduor5742@gmail.com>
2021-03-01 15:59:59 +0300

    created login function by defining new_pitch function

--- app/main/views.py
+++ app/main/views.py
@@ -16,3 +16,25 @@ def index():
     message= "Welcome to Pitch Application!!"
     title= 'Pitch-app!'
     return render_template('index.html', message=message,title=title)
+
+@main.route('/pitch/', methods = ['GET','POST'])
+@login_required
+def new_pitch():
+
+    form = PitchForm()
+
+    if form.validate_on_submit():
+        category = form.category.data
+        pitch= form.pitch.data
+        title=form.title.data
+
+        # Updated pitchinstance
+        new_pitch = Pitches(title=title,category= category,pitch= pitch,user_id=current_user.id)
+
+        title='New Pitch'
+
+        new_pitch.save_pitch()
+
+        return redirect(url_for('main.index'))
+
+    return render_template('pitch.html',form= form)
