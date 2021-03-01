962b5eeeb711678f52126a3d0987fd3b4e5e2678
nicholas-oduor <oduor5742@gmail.com>
2021-03-01 16:48:55 +0300

    added class User and class Role methods

--- app/models.py
+++ app/models.py
@@ -30,4 +30,42 @@ class Pitches(db.Model):
 
     def __repr__(self):
         return f"Pitches {self.pitch}','{self.date}')"
+
+class User(UserMixin,db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer,primary_key = True)
+    author = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    pitch = db.relationship('Pitches', backref='author', lazy='dynamic')
+    pass_secure = db.Column(db.String(255))
+    
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+    
+    def __repr__(self):
+        return f'User {self.author}'
   
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'role',lazy="dynamic")
+    
+
+    def __repr__(self):
+        return f'User {self.name}'
+
