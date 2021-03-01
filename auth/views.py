e23a4db4713ac665191cd09a320165abf341d3b3
nicholas-oduor <oduor5742@gmail.com>
2021-03-01 15:29:34 +0300

    added logout function to enable user to logout

--- auth/views.py
+++ auth/views.py
@@ -35,3 +35,10 @@ def register():
         return redirect(url_for('auth.login'))
         title = "New Account"
     return render_template('auth/register.html',registration_form = form)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been successfully logged out')
+    return redirect(url_for("main.index"))
