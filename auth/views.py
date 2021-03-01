fd5ab9431dfb2c68554eaf6787738a13cecdaa54
nicholas-oduor <oduor5742@gmail.com>
2021-03-01 15:28:01 +0300

    added register function

--- auth/views.py
+++ auth/views.py
@@ -20,3 +20,18 @@ def login():
 
     title = "Pitch Application login"
     return render_template('auth/login.html',login_form = login_form,title=title)
+
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        print('password', form.password.data)
+        user = User(email = form.email.data, author = form.username.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+
+        mail_message("Welcome to Pitch Application","email/welcome_user",user.email,user=user)
+
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+    return render_template('auth/register.html',registration_form = form)
