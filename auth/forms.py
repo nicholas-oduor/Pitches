085248fa30494d19da49b1486289b09e07531aca
nicholas-oduor <oduor5742@gmail.com>
2021-03-01 15:21:31 +0300

    validated username function

--- auth/forms.py
+++ auth/forms.py
@@ -14,3 +14,7 @@ class RegistrationForm(FlaskForm):
     def validate_email(self,data_field):
         if User.query.filter_by(email =data_field.data).first():
             raise ValidationError('There is an account with that email')
+    
+    def validate_username(self,data_field):
+        if User.query.filter_by(author = data_field.data).first():
+            raise ValidationError('That username is taken')
